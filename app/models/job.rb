53282c49342076ba15da6aca366b6810b346b0e6
JPlante9117 <jacrplante@gmail.com>
2020-03-06 14:24:03 -0500

    added has many relationships

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+    has_many :job_labels
+    has_many :labels, through: :job_labels
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,4 @@
 class Label < ApplicationRecord
+    has_many :job_labels,
+    has_many :jobs, through: :job_labels
 end
