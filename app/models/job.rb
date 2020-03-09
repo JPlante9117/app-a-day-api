6360f606acaceb6152f1a8218c3ffef7e1bbbed8
JPlante9117 <jacrplante@gmail.com>
2020-03-09 11:10:01 -0400

    updates job labels on update

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,17 +2,17 @@ class Job < ApplicationRecord
     has_many :job_labels
     has_many :labels, through: :job_labels, autosave: true
 
-    # validates :title, presence: true
-    # validates :description, presence: true
-    # validates :status, presence: true
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :status, presence: true
 
     accepts_nested_attributes_for :labels
 
-    before_save :find_or_create_label
-
-    def find_or_create_label
-        self.labels = self.labels.map do |label|
-            Label.find_or_create_by(title: label.title)
+    def labels_attributes=(label_attr)
+        self.labels.clear
+        label_attr.each do |label_item|
+            label = Label.find_or_create_by(title: label_item[:title])
+            self.labels << label
         end
     end
 end
