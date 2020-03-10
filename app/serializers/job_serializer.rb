702cffb2b6292afd624c20c65f252fc202f7b486
JPlante9117 <jacrplante@gmail.com>
2020-03-10 15:24:50 -0400

    goal serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
     has_many :labels, through: :job_labels
 
-    attributes :id, :title, :description, :status, :link, :labels
+    attributes :id, :title, :description, :status, :link, :updated_at, :labels
 end
